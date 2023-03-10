afe36829c2d7aef5bf12e0f20d5db489bddba029
Matheus Teles <mateus.steles13@gmail.com>
2023-03-10 15:08:18 -0300

    Update delete text font

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def login():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text='Login System', text_font=("Roboto", 24))
+label = customtkinter.CTkLabel(master=frame, text='Login System')
 label.pack(pady=12, padx=10)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
